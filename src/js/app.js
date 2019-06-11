f0d74534074b0fd98db4c0cd94ab903a698fc18d
simonequarticelli <quarti87@gmail.com>
2019-06-11 19:43:28 +0200

    esercizio finito

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,24 @@
 var $ = require('jquery'); //<-- prendi jquery e crea variabile dollaro
 
 $(document).ready(function(){
+  //nascondo il dropdown-menu e la seconda nav
   $('.dropdown').hide();
+  $('.header__nav_2').hide();
 
+  //dropdown-menu
   $('.header__nav__menu > ul > li:first-child').mouseenter(function(event) {
     $('.dropdown').fadeIn(100);
   }).mouseleave(function(event) {
     $('.dropdown').fadeOut(100);
   });
 
-
-  // if ($('html').scrollTop() > 100) {
-  //   $('.header__nav_2').show();
-  // }else{
-  //   $('.header__nav_2').hide();
-  // }
-
-
-
-
-
-
+  //controllo lo scroll della finestra
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 350) {
+      $('.header__nav_2').slideDown(200);
+    }else{
+      $('.header__nav_2').slideUp(200);
+    }
+  })
 
 });
